294ebd333017ce39d0a6843893ee598965871d38
J092448 <ljh0924487@gmail.com>
2025-02-13 13:24:35 +0900

    테스트
    테스트

---
+++ juhyeon/src/main/java/com/icia/juhyeon/JuhyeonApplication.java
@@ -0,0 +1,13 @@
+package com.icia.juhyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuhyeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuhyeonApplication.class, args);
+    }
+
+}
---
+++ juhyeon/src/test/java/com/icia/juhyeon/JuhyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.juhyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuhyeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
